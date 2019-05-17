bfcc6947cedafb4204560d7c1629bf0e750e4cea
颜阳 <41497433+mmmmyan@users.noreply.github.com>
2019-05-17 11:46:47 +0800

    1
    1

---
+++ springboot-rabbitmq-01/src/test/java/com/debug/steadyjack/AppTest.java
@@ -0,0 +1,17 @@
+package com.debug.steadyjack;
+
+import com.debug.steadyjack.service.InitService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+    @Autowired
+    private InitService initService;
+
+
+}
